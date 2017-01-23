758347f27cd2d9a4afffdb95f5a3c8bb1b0394da
Marko Laine <marko.laine@gmail.com>
2017-01-23 09:02:17 +0200

    Initial commit

---
+++ R/f90kriege.R
@@ -0,0 +1,231 @@
+### Fast Krieging by calling fortran Krieging code from R
+### This only works with MOS temperature data in predefined format
+###
+### compilation of the shared code by a command like:
+### R CMD SHLIB kriegecode.f90 -lRblas -lRlapack
+###
+### or install as package:
+### R CMD INSTALL fastgrid_n.n.tar.gz
+
+### marko.laine@fmi.fi
+
+## This loads the code to R memory
+# if (!is.loaded("kriegecode")) {
+#    dyn.load("kriegecode.so")
+#}
+
+#require(Matrix) # for sparseMatrix
+#require(sp) # for spatial objects
+
+## cover function for fortran kriging code in kriegecode.f90
+f90kriege <- function(x,y,b,grid,cy,cgrid,covpars) {
+    nobs <- nrow(x)
+    npar <- ncol(x)
+    ngrid <- nrow(grid)
+
+    ## There is very little checks in the Fortran code, so check the sanity of the inputs here 
+    if (nrow(y) != nobs | ncol(y) != 1 | nrow(cy) != nobs
+        | ncol(grid) != npar | nrow(cgrid) != ngrid
+        | ncol(cy) != 2 | ncol(cgrid) != 2
+        | !is.numeric(covpars) | length(covpars) < 2) {
+        stop("input dimensions do not match")
+    }
+    ## Call the external f90 code for Kriging predictions    
+    ypred <- .Fortran("kriegepred",
+                      x=as.double(x),
+                      y=as.double(y),
+                      b=as.double(b),
+                      grid=as.double(grid),
+                      ypred=double(length=ngrid),
+                      cy=as.double(cy),
+                      cgrid=as.double(cgrid),
+                      nobs=as.integer(nobs),
+                      npar=as.integer(npar),
+                      ngrid=as.integer(ngrid),
+                      covpars=as.double(covpars)
+                      )$ypred
+
+    if (b[1,1] == -1.0) {
+        stop("Problems with the covariance information, no prediction done")
+    }
+    
+    return(ypred)
+}
+
+### cover function for fortran code for observation operator
+f90Hmat <- function(mlat,mlon,obs) {
+  nobs <- nrow(obs)
+  nlat <- length(mlat)
+  nlon <- length(mlon)
+  nmod <- nlat*nlon
+
+  if (nlat < 1 | nlon < 1 | ncol(obs) != 2 | !is.numeric(obs) )
+      stop("input dimensions do not match")
+  
+  f90h <- .Fortran("obsoper",                   
+                   mlat=as.double(mlat),
+                   mlon=as.double(mlon),
+                   obs=as.double(obs),
+                   nlat=as.integer(nlat),
+                   nlon=as.integer(nlon),
+                   nobs=as.integer(nobs),
+                   iind=integer(length=4*nobs),
+                   jind=integer(length=4*nobs),
+                   h=double(length=4*nobs)
+                   )
+
+  return(sparseMatrix(i=f90h$iind,j=f90h$jind,x=f90h$h,dims=c(nobs,nmod)))
+}
+
+## to replace gstat kriege command for prediction over a grid
+fastkriege <- function(trend_model, data, grid, cov.pars, bg=NULL,variable="temperature" ) {
+    ## build input matrices
+    ## assumes data and bg have "longitude", "latitude", "temperature", and data and grid also trend model variables
+
+    trend_model_noy <- trend_model[-2]
+    grid.variables <- all.vars(trend_model_noy)
+  
+    if (!is.null(bg)) {
+      ## map from from bg grid to station locations
+      ## need the grid for obs operator, FIX me
+      s<-summary(grid)$grid
+      elon<-seq.int(from=s[1,1],by=s[1,2],len=s[1,3])
+      elat<-seq.int(from=s[2,1]+s[2,2]*(s[2,3]-1) ,by=-s[2,2],len=s[2,3])
+      H<-f90Hmat(elon,elat,cbind(data$longitude,data$latitude))
+#      mu <- as.matrix(H%*%bg$temperature)
+      mu <- as.matrix(H%*%as.matrix(bg@data[variable]))
+    }
+    else {
+        mu <- 0.0
+    }
+
+    ## non missing of all trend model variables
+    igrid <- complete.cases(as.data.frame(grid)[,grid.variables])
+  
+    B <- buildcovmat(coords=coordinates(data), cov.model = "exp", cov.pars=cov.pars)
+    X <- model.matrix(trend_model_noy, data=data)
+#    y <- as.matrix(data$temperature-mu)
+    y <- as.matrix(data@data[variable]-mu)
+    predgrid<-model.matrix(trend_model_noy,data=grid)
+    cy <- as.matrix(coordinates(data))
+    cgrid <- as.matrix(coordinates(grid))[igrid,]
+    
+    ## Kriging by fortran code
+    t1<-proc.time()
+    ypred<-f90kriege(X,y,B,predgrid,cy,cgrid,cov.pars)
+    t2<-proc.time()-t1
+
+    ypredgrid<-double(length=nrow(grid))*NA
+    ypredgrid[igrid]<-ypred
+  
+    ## add bg field to the results
+    if (is.null(bg))
+        ypred2<-data.frame(temperature=ypredgrid)
+    else
+  #      ypred2<-data.frame(temperature=ypredgrid+bg$temperature)
+       ypred2<-data.frame(temperature=ypredgrid+bg@data[variable])
+    names(ypred2) <- c(variable)
+    coordinates(ypred2)<-coordinates(grid)
+    gridded(ypred2)<-TRUE
+    return(ypred2)
+}
+
+
+### Observation operator using bi-linear interpolation and bisection
+### Returns sparse matrix
+### See also: f90Hmat, which uses fortran code
+Hmat <- function(mlat,mlon,obs) {
+    nobs <- nrow(obs)
+    nlat <- length(mlat)
+    nlon <- length(mlon)
+    nmod <- nlat*nlon
+    ## sparse matrix
+    H<-Matrix(0,nrow=nobs,ncol=nmod,sparse=TRUE)
+  
+    for (i in 1:nobs) {
+        olat <- obs[i,1]
+        olon <- obs[i,2]
+        i1i2 <- lookup(mlat,olat)
+        j1j2 <- lookup(mlon,olon)
+        i1<-i1i2[1]
+        i2<-i1i2[2]
+        j1<-j1j2[1]
+        j2<-j1j2[2]
+        I1 <- sub2ind(c(nlat,nlon),i1,j1)
+        I2 <- sub2ind(c(nlat,nlon),i2,j1)
+        I3 <- sub2ind(c(nlat,nlon),i1,j2)
+        I4 <- sub2ind(c(nlat,nlon),i2,j2)
+        H[i,c(I1,I2,I3,I4)] <- intcoef(olat,olon,mlat[i1],mlat[i2],mlon[j1],mlon[j2])
+    }
+    H
+}
+
+### utility for Hmat
+sub2ind<-function(nrc,irow,icol) {
+    (icol-1)*nrc[1] + irow
+}
+
+### calculate interpolation coefficients for bi-linear 2D interpolation
+intcoef <- function(x1,x2,x1l,x1u,x2l,x2u) {
+    X <- matrix(data=c(1,0,0,0,-1,1,0,0,-1,0,1,0,1,-1,-1,1),nrow=4,ncol=4,byrow=TRUE)
+    x11 <- (x1-x1l)/(x1u-x1l)
+    x22 <- (x2-x2l)/(x2u-x2l)
+    h <- matrix(data=c(1,x11,x22,x11*x22),nrow=1,ncol=4)%*%X
+    h
+}
+
+### table lookup by bisection 
+lookup<-function(x,xi) {
+    n <- length(x)
+    if (x[2]-x[1] > 0) {
+        l <- 1
+        u <- n
+        while (u-l > 1) {
+            m <- floor((u+l)/2)
+            if (xi>x[m]) l<-m
+            else  u<-m
+        }
+        return(c(l,u))
+    } else {
+        l <- n
+        u <- 1
+        while (l-u > 1) {
+            m <- floor((u+l)/2)
+            if (xi>x[m]) l<-m
+            else  u<-m
+        }
+        return(c(u,l))
+    }
+}
+
+### calculate covariance matrix given data coordinates and spatial parameters
+### only exponential covariance function implemented so far
+### TODO: use spDists function from sp
+buildcovmat<-function (coords = NULL, cov.model = "exp",  
+                    cov.pars = stop("no cov.pars argument"), dist.method = "euclidian", ...) {
+    if (is.null(coords))
+        stop("coords missing")
+    n <- nrow(coords)
+    sigmasq <- cov.pars[1]
+    phi <- cov.pars[2]
+    if (length(cov.pars)>2) 
+        nugget <- cov.pars[3]
+    else
+        nugget <- 0.0
+    tausq <- nugget
+    dists.lowertri <- as.vector(dist(coords,method=dist.method))
+    if (round(1e+12 * min(dists.lowertri)) == 0) 
+        warning("Two or more pairs of data at coincident (or very close) locations. \nThis may cause crashes in some matrices operations.\n")
+    varcov <- matrix(0, n, n)
+    if (all(sigmasq < 1e-10) | all(phi < 1e-10)) {
+        varcov <- diag(x = (tausq + sum(sigmasq)), n)
+    }
+    else {
+        covvec <- exp(-(dists.lowertri/phi))*sigmasq
+        varcov[lower.tri(varcov)] <- covvec
+        varcov <- t(varcov)
+        varcov[lower.tri(varcov)] <- covvec
+        diag(varcov) <- tausq + sigmasq
+    }
+    return(varcov)
+}
---
+++ src/kriegecode.f90
@@ -0,0 +1,248 @@
+!!! Krieging code to speed up computations done in R
+
+!!! R compile:
+!!! R CMD SHLIB kriegecode.f90 -lRblas -lRlapack
+
+!!! this version does not use shape information and does not have internal checks
+
+!! kriging predictions as a linear model
+!! y = x*beta + eps, eps ~ N(0,b)
+!! ypred = xgrid*beta + Bgrid*B^(-1)(y-x*beta)
+!! cy, cgrid coordinates of data and the grid
+subroutine kriegepred(x,y,b,grid, ypred, cy,cgrid,nobs,npar,ngrid, covpars)
+  implicit none
+  
+  !  include 'lapack_inc.f90'
+  integer, parameter :: Double = selected_real_kind(15)
+  integer, parameter :: dbl=Double
+  integer, parameter :: ncovpars = 2
+
+  integer, intent(in) :: nobs, npar, ngrid
+  real(kind=dbl), intent(inout) :: x(nobs,npar), y(nobs,1), b(nobs,nobs), grid(ngrid,npar), ypred(ngrid)
+  real(kind=dbl), intent(in) :: cy(nobs,2), cgrid(ngrid,2)
+  real(kind=dbl), intent(in) :: covpars(ncovpars)
+  
+  real(kind=dbl) :: beta(npar,1)
+  real(kind=dbl) :: x1(npar), b1(nobs)
+    
+  real(kind=dbl) :: ypred1(1)
+    
+  integer :: i
+
+  real(kind=dbl), parameter :: one = 1.0d0, zero = 0.0d0
+   
+  call chol(b, nobs)               ! replace B with L = chol(B)
+  call ldivide(b,x, nobs, npar)    ! replace x with L\x
+  call ldivide(b,y, nobs, 1)       ! replace y with L\y
+  call lsq(x,y,beta, nobs, npar)   ! beta = x\y
+  call resid(x,y,beta, nobs, npar) ! y = y - x*beta
+  call ltdivide(b,y, nobs, 1)      ! y = L'\y
+
+  ! calculate prediction over grid, ypred(i) = X1*beta + B1*B\(y-X*beta)
+  do i=1, ngrid
+     x1 = grid(i,:)
+     call corrdist(cgrid(i,:),cy,b1, nobs, covpars)
+     ypred1 = matmul(x1,beta) + matmul(b1,y)
+     ypred(i) = ypred1(1)
+  end do
+
+contains
+!!! Utility functions to call LAPACK and BLAS library routines for some matrix computations.
+!!! The utilites do not use shape information, so they can be called from R.
+!!! Calls: dpotrf, dtrsm, dgels, dgemv
+  
+  !! correlation calculations
+  subroutine corrdist(x1,x,b1, n, covpars)
+    implicit none
+    real(kind=dbl), intent(in) :: x1(:), x(:,:)
+    integer, intent(in) :: n
+    real(kind=dbl), intent(out) :: b1(n)
+    real(kind=dbl), intent(in) :: covpars(ncovpars)
+    
+    integer :: i
+    real(kind=dbl) :: d
+    real(kind=dbl) :: sig2, clen
+    
+    sig2 = covpars(1)
+    clen = covpars(2)
+    
+    do i=1,n
+       d = sqrt((x1(1)-x(i,1))**2 + (x1(2)-x(i,2))**2)
+       b1(i) = exp(-d/clen)*sig2
+    end do  
+  end subroutine corrdist
+
+  !! X = chol(X,'lower'), X(1,1) == -1 on error
+  subroutine chol(x,n)
+    implicit none
+    integer, intent(in) :: n
+    real(kind=dbl),intent(inout) :: x(n,n)
+
+    integer :: info
+
+    if (n<1) return
+    call dpotrf('l',n,x,n,info)
+
+    if (info .ne. 0) then
+       x(1,1) = -1.0d0      
+       if (n > 1) x(1,2) = info
+    end if    
+  end subroutine chol
+
+  !! X = L\X, L is lower chol, replaces X
+  subroutine ldivide(l,x, n, m)
+    implicit none
+    integer, intent(in) :: n, m
+    real(kind=dbl),intent(in) :: l(n,n)
+    real(kind=dbl),intent(inout) :: x(n,m)
+
+    call dtrsm('left','l','n','n',n,m,one,l,n,x,n)
+  end subroutine ldivide
+
+  !! X = L'\X, L is lower chol, replaces X
+  subroutine ltdivide(l,x, n,m)
+    implicit none
+    integer, intent(in) :: n, m
+    real(kind=dbl),intent(in) :: l(n,n)
+    real(kind=dbl),intent(inout) :: x(n,m)
+
+    call dtrsm('left','l','t','n',n,m,one,l,n,x,n)
+  end subroutine ltdivide
+
+  !! beta = x\y, general least squares solution, now assumes size(y,2) == 1!
+  subroutine lsq(x,y,beta, m, n) ! oops n and m in wrong way!!
+    implicit none
+    integer, intent(in) :: n, m
+    real(kind=dbl), intent(in) :: x(m,n)
+    real(kind=dbl), intent(in) :: y(m,1)
+    real(kind=dbl), intent(out) :: beta(n)
+
+    integer :: nrhs, lwork, info
+    real(kind=dbl), allocatable :: work(:), x2(:,:), y2(:,:)
+    
+    nrhs = 1 ! size(y,2)
+    lwork = min(m,n)+max(1,m,n,nrhs)
+    allocate(work(lwork), x2(m,n), y2(m,nrhs))
+    y2 = y
+    x2 = x
+    call dgels('n', m, n, nrhs, x2, m, y2, m, work, lwork, info)
+    if (info .ne. 0) then
+!       write(*,*) 'dgels failed'
+!       write(*,*) 'info = ', info
+    endif
+    beta = y2(1:n,1)
+    deallocate(work,x2,y2)
+  end subroutine lsq
+
+  !! residuals y = y-x*beta, replaces y
+  subroutine resid(x,y,beta, n,m)
+    implicit none
+    integer, intent(in) :: m,n
+    real(kind=dbl), intent(in) :: x(n,m)
+    real(kind=dbl), intent(inout) :: y(n,1)
+    real(kind=dbl), intent(in) :: beta(m)
+    call dgemv('n',n,m,-one,x,n,beta,1,one,y,1)
+  end subroutine resid
+    
+end subroutine kriegepred
+
+subroutine obsoper(mlat,mlon,obs,nlat,nlon,nobs,iind,jind,h)
+  implicit none
+
+  integer, parameter :: Double = selected_real_kind(15)
+  integer, parameter :: dbl=Double
+
+  integer, intent(in) :: nlat, nlon, nobs
+  real(kind=dbl), intent(in) :: mlat(nlat), mlon(nlon), obs(nobs,2)
+
+  integer, intent(out) :: iind(4*nobs), jind(4*nobs)
+  real(kind=dbl), intent(out) :: h(4*nobs)
+
+  integer :: nmod, i, ii, j, i1i2(2),j1j2(2), i1,i2,j1,j2, ii4(4)
+  real(kind=dbl) :: olat, olon, h4(4)
+
+  nmod = nlat*nlon
+!  # sparse matrix
+!  H<-Matrix(0,nrow=nobs,ncol=nmod,sparse=TRUE)
+
+  j = 1
+  do i = 1,nobs
+    olat = obs(i,1)
+    olon = obs(i,2)
+    i1i2 = lookup(mlat,olat)
+    j1j2 = lookup(mlon,olon)
+    i1 = i1i2(1)
+    i2 = i1i2(2)
+    j1 = j1j2(1)
+    j2 = j1j2(2)
+    II4(1) = sub2ind(nlat,i1,j1)
+    II4(2) = sub2ind(nlat,i2,j1)
+    II4(3) = sub2ind(nlat,i1,j2)
+    II4(4) = sub2ind(nlat,i2,j2)
+    h4 = intcoef(olat,olon,mlat(i1),mlat(i2),mlon(j1),mlon(j2))
+    do ii=1,4
+       h(j) = h4(ii)
+       iind(j) = i
+       jind(j) = II4(ii)
+       j = j + 1
+    end do
+ end do
+ 
+contains
+  
+  function intcoef(x1,x2,x1l,x1u,x2l,x2u) result(h)
+    implicit none
+    real(kind=dbl), intent(in) :: x1,x2,x1l,x1u,x2l,x2u
+    real(kind=dbl) :: h(4)
+    
+    real(kind=dbl) :: X(4,4), x11, x22
+
+    X = reshape((/1.0,-1.0,-1.0,1.0,0.0,1.0,0.0,-1.0,0.0,0.0,1.0,-1.0,0.0,0.0,0.0,1.0/),(/4,4/))
+    x11 = (x1-x1l)/(x1u-x1l)
+    x22 = (x2-x2l)/(x2u-x2l)
+    h = reshape(matmul(reshape((/1.0d0,x11,x22,x11*x22/),(/1,4/)),X),(/4/))
+  end function intcoef
+
+  function sub2ind(nrc,irow,icol) result(i)
+    implicit none
+    integer, intent(in) :: nrc, irow, icol
+    integer :: i
+    i = (icol-1)*nrc + irow
+  end function sub2ind
+
+  function lookup(x,xin) result(ii)
+    implicit none
+    real(kind=dbl) :: x(:), xin
+    integer :: ii(2)
+    integer :: n, il, iu, im
+
+    n = size(x,1)
+    !!  find location by bisection
+    if (x(2)-x(1)>0.0) then
+       il=1
+       iu=n
+       do while (iu-il>1)
+          im = floor((iu+il)/2.0)
+          if (xin>x(im)) then
+             il=im
+          else
+             iu=im
+          end if
+       end do
+       ii = (/il,iu/)
+    else
+       il=n
+       iu=1
+       do while (il-iu>1)
+          im = floor((iu+il)/2.0)
+          if (xin>x(im)) then
+             il=im
+          else
+             iu=im
+          end if
+       end do
+       ii = (/iu,il/)
+    end if
+  end function lookup
+
+end subroutine obsoper
