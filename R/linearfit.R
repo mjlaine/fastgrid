27e2b5685b229f55cb20e18a7bdce988f89fb52b
Marko Laine <marko.laine@gmail.com>
2018-01-24 13:23:21 +0200

    linearinterpolationtogrid, test routine

--- R/linearfit.R
+++ R/linearfit.R
@@ -3,9 +3,8 @@
 
 
 # this uses sparse Cholesky
-
 #' @export 
-linearinterpolationtogrid <- function(obspoints, modelgrid, sigobs=1.0, sigmodel=1.0, lam=c(1,1), d=2,
+linearinterpolationtogrid2 <- function(obspoints, modelgrid, sigobs=1.0, sigmodel=1.0, lam=c(1,1), d=2,
                    priormodel = 0.0, method = 'bilinear', variable = 'temperature') {
   elon <- gridlon(modelgrid)
   elat <- gridlat(modelgrid)
@@ -24,18 +23,18 @@ linearinterpolationtogrid <- function(obspoints, modelgrid, sigobs=1.0, sigmodel
   H4 <- Diagonal(n*m)/sigmodel
   HH <- rbind(H1,H2,H3,H4)
   yy <- rbind(y,matrix(0.0,nrow=nrow(H2)+nrow(H3)),matrix(c(priormodel),nrow=m*n))
-  R<-chol(t(HH)%*%HH)
-  x<-backsolve(R,backsolve(R,t(HH)%*%yy,transpose=TRUE),transpose=FALSE)
+  R<-chol(crossprod(HH))
+  x<-backsolve(R,backsolve(R,crossprod(HH,yy),transpose=TRUE),transpose=FALSE)
 #  out <- modelgrid
 #  out@data[,variable] <- x
   dim(x)<-c(n,m)
   return(x)
 }
 
-# this is as slow or slower
-#' @export 
-linearinterpolationtogrid2 <- function(obspoints, modelgrid, sigobs=1.0, sigmodel=1.0, lam=c(1,1), d=2,
-                                      priormodel = 0.0, method = 'bilinear', variable = 'temperature') {
+# Using sparse solve
+#' @export
+linearinterpolationtogrid <- function(obspoints, modelgrid, sigobs=1.0, sigmodel=1.0, lam=c(1,1), d=2,
+                                       priormodel = 0.0, method = 'bilinear', variable = 'temperature') {
   elon <- gridlon(modelgrid)
   elat <- gridlat(modelgrid)
   m <- length(elon)
@@ -51,16 +50,11 @@ linearinterpolationtogrid2 <- function(obspoints, modelgrid, sigobs=1.0, sigmode
   H2 <- Diagonal(n)%x%diff(Diagonal(m),differences=d) * lam[1]/londx**d
   H3 <- diff(Diagonal(n),differences=d)%x%Diagonal(m) * lam[2]/latdx**d
   H4 <- Diagonal(n*m)/sigmodel
-# HH <- rbind(H1,H2,H3,H4)
-#  yy <- rbind(y,matrix(0.0,nrow=nrow(H2)+nrow(H3)),matrix(c(priormodel),nrow=m*n))
-#  R<-chol(t(HH)%*%HH)
-  R <- chol(t(H1)%*%H1 + t(H2)%*%H2 + t(H3)%*%H3 + t(H4)%*%H4)
-#  x<-backsolve(R,backsolve(R,t(HH)%*%yy,transpose=TRUE),transpose=FALSE)
-  x<-backsolve(R,backsolve(R,t(H1)%*%y + t(H4)%*%matrix(c(priormodel),nrow=m*n),transpose=TRUE),transpose=FALSE)
+  x <- solve(crossprod(H1) + crossprod(H2) + crossprod(H3) + crossprod(H4), crossprod(H1,y) + crossprod(H4,matrix(c(priormodel),nrow=m*n)))
+  dim(x)<-c(n,m)
+  x<-as.matrix(x)
   #  out <- modelgrid
   #  out@data[,variable] <- x
-  dim(x)<-c(n,m)
   return(x)
 }
-
 
